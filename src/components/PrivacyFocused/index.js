0471101689d208f1e376002382b44f4692a08cee
CosmicAnon <cosmicanon1320@gmail.com>
2021-06-02 00:24:55 +0530

    updates

--- src/components/PrivacyFocused/index.js
+++ src/components/PrivacyFocused/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import { Font } from "..";
-import { floatingIcon, background } from "../../images";
+import { background } from "../../images";
 import { PrivacyLeftSide } from "./PrivacyLeftSide";
 
 
