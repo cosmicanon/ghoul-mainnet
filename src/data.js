fc70ed6389e2d7e05c6563d74f19968bda1c53a4
CosmicAnon <cosmicanon1320@gmail.com>
2021-06-22 12:26:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const url =
   process.env.NODE_ENV === "production"
     ? // ? "https://ghoultoken.crypto"
-      "https://zen-hodgkin-d0c4ae.netlify.app/"
+      "https://ghoultoken.io"
     : "http://localhost:8000";
 
 export const darkpaper = url + "/ghoul_ecosystem_dark_paper.pdf";
