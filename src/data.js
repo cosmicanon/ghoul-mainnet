fe0782b86c7bcfbe807a6b5e0e2455bd4e1d6df2
CosmicAnon <cosmicanon1320@gmail.com>
2021-06-24 13:48:03 +0530

    updates

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const url =
   process.env.NODE_ENV === "production"
     ? // ? "https://ghoultoken.crypto"
-      "/"
+      ""
     : "http://localhost:8000";
 
 export const darkpaper = url + "/ghoul_ecosystem_dark_paper.pdf";
